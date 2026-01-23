2afb3c7af122fd2c74570dc2fdb73d089a942b85
Jochen Hagenstroem <jhagenst@adobe.com>
2026-01-23 14:01:51 -0800

    Speed insights for vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import ImageGallery from './components/ImageGallery'
 import './App.css'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 
 function App() {
   return (
     <div className="app">
       <ImageGallery />
+      <SpeedInsights />
     </div>
   )
 }
